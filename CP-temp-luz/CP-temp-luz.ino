54a6776c47f1e80b5e6374ebb00d3b79891c2c33
Ana Martins <ana@ana-901.(none)>
2016-11-29 22:40:02 +0000

    Add control for brightness with the buttons

--- CP-temp-luz/CP-temp-luz.ino
+++ CP-temp-luz/CP-temp-luz.ino
@@ -6,17 +6,22 @@
 #define TEMPIN A0
 int tempMax = analogRead(TEMPIN)+1;
 int tempMin = analogRead(TEMPIN)-1;
+bool leftButtonPressed;
+bool rightButtonPressed;
+int brilho = 128;
 
 void setup() {
   Serial.begin(115200);
   CircuitPlayground.begin();
   CircuitPlayground.clearPixels();
+  CircuitPlayground.setBrightness(brilho);
+
   delay(1000);
   }
 
 void loop() {
-  uint16_t tempValue = analogRead(TEMPIN);
   
+  uint16_t tempValue = analogRead(TEMPIN);
   if (tempMax < tempValue){
     tempMax = tempValue + 1;
   }
@@ -27,6 +32,23 @@ void loop() {
     tempMin = 0;
   }
 
+  leftButtonPressed = CircuitPlayground.leftButton();
+  rightButtonPressed = CircuitPlayground.rightButton();
+  
+  if (leftButtonPressed) {
+    brilho = brilho - 20;
+  } 
+  if (rightButtonPressed) {
+    brilho = brilho + 20;
+  }
+  if (brilho > 248 ) {
+    brilho = 255;
+  }
+  if (brilho < 1) {
+   brilho = 1;
+  }
+   
+  CircuitPlayground.setBrightness(brilho);  
   int tempMapped = map(tempValue, tempMin, tempMax, 0, 255);
 
 for(int i=10; i>=0; --i) {
@@ -37,6 +59,7 @@ for(int i=10; i>=0; --i) {
 //Serial.println(tempMapped);
 //Serial.println(tempMin);
 //Serial.println(tempMax);
+Serial.println(brilho);
 //Serial.println("XXXXXXXXXXXXXX");
  
  delay(500);
